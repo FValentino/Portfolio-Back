7fc092ac7ea31545e7525dfad1e32f9f2557ad23
Valentino Farias <fariasvalentino25@gmail.com>
2023-05-18 13:29:12 -0300

    CORS arreglado

--- src/main/java/com/fariasvalentino/Portfolio/controller/PortfolioController.java
+++ src/main/java/com/fariasvalentino/Portfolio/controller/PortfolioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController; 
 
-@CrossOrigin ("portfolio-926aa.firebaseapp.com")
+@CrossOrigin 
 @RestController
 public class PortfolioController {
     
